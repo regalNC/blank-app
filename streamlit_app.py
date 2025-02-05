9cfe8aaa267d741e691df0dac6f68fa49a264c79
regalNC <37859580+regalNC@users.noreply.github.com>
2025-02-05 18:43:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new app2")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's xxx start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
