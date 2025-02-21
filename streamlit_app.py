ac7e3899e6740af74a2eb310ab519f464cebc71f
regalNC <37859580+regalNC@users.noreply.github.com>
2025-02-21 14:51:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,63 +3,8 @@ import pandas as pd
 import re
 import streamlit as st
 
-st.write("Start")
-def extract_pcap_info(pcap_file):
-    # Read packets from the PCAP file
+st.write("Start") 
 
-    ips_src   = []
-    ips_dst   = []
-    ports_src = []
-    ports_dst = []
-    fnames    = []
-    pkt_nums  =[]
-
-    packets = rdpcap(pcap_file)
-
-    # Iterate through packets and extract relevant information
-
-    for i, packet in enumerate(packets):
-        #pbar.progress(i + 1, text="Operation in progress")
-
-        # IP layer
-        if packet.haslayer("IP"):
-            ip = packet.getlayer("IP")
-            ipsrc = ip.src
-            ipdst = ip.dst
-
-        # TCP layer
-        if packet.haslayer("TCP"):
-            tcp = packet.getlayer("TCP")
-            payload = bytes(tcp.payload)
-            payload_text = payload.decode("utf-8", errors="ignore")
-            #if re.search(r"(GET|POST|HTTP/1\.[01])", payload_text):
-            if re.search(r"(^GET\s|^POST\s|^PUT\s)", payload_text):
-                print(f"Packet {i + 1}:")
-                header = payload_text.split("\n")[0]
-                #print(header)
-                pkt_nums.append(i+1)
-                ips_src.append(ipsrc)
-                ips_dst.append(ipdst)
-                ports_src.append(tcp.sport)
-                ports_dst.append(tcp.dport)
-                file_name = header.split()[1]
-                if "/" in file_name:
-                    fnames.append(payload_text.split()[1].split("/")[-1])
-                else:
-                    fnames.append(payload_text.split()[1])
-
-                print(f" IP src: {ipsrc}")
-                print(f" IP dst: {ipdst}")
-                print(f" Source Port: {tcp.sport}")
-                print(f" Destination Port: {tcp.dport}")
-                print()
-
-    data = {'pkt #': pkt_nums,'malware': fnames, 'ip src':ips_src, 'ip dst':ips_dst,
-            'src port':ports_src, 'dst port':ports_dst}
-
-    # Create DataFrame
-    df = pd.DataFrame(data)
-    return(df)
 
 st.header("Malware expander")
 
